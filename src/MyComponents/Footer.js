9a1641b39a65dd384193a1d67198f4157c60bd4f
unknown <abhishekfasate@gmail.com>
2022-01-07 16:23:35 -0800

    Commited succesfully

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+export const Footer = () => { 
+    return (
+        <footer className="bg-dark text-light py-3">
+            <p className="text-center">
+            Copyright &copy; a_b_h_i_s_h_e_k_f
+            </p>
+        </footer>
+    )
+}
